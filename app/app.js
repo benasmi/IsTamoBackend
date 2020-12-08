5d15575ea2026892464af10c3506a235f5eaec2a
Jokūbas Setkauskas <graphframejokubas@gmail.com>
2020-12-08 11:45:24 +0200

    Structural changes

--- app/app.js
+++ app/app.js
@@ -3,27 +3,18 @@ const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, './config/.env')});
 
-// create express app
 const app = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-// define a root route
-app.get('/', (req, res) => {
-  res.send("Hello World");
-});
+const genericRoutes = require('./routes')
+const userRoutes = require('./routes/userRoutes')
 
-// Require employee routes
-const userRoutes = require('./repository/userRepository')
+app.use(genericRoutes)
+app.use('/api/users', userRoutes)
 
-// using as middleware
-app.use('/api/v1/users', userRoutes)
-
-// listen for requests
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
 });
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -2,6 +2,5 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController');
 
-// Create a new employee
 router.post('/', userController.create);
 module.exports = router
