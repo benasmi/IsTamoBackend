e607fbd8d19e488efdb206428840ae562b77c06c
Jokūbas Setkauskas <graphframejokubas@gmail.com>
2020-12-11 13:59:41 +0200

    Added subject related CRUD ops

--- app/models/subject.js
+++ app/models/subject.js
@@ -10,4 +10,26 @@ Subject.getSubject = async (data) => {
     return subject[0]
 }
 
+Subject.createSubject = async (data) => {
+    const [result,fields] = await dbConn.query("INSERT INTO SUBJECT " +
+    "(name, description, hours) VALUES " +
+    "(?,?,?)", [data.name, data.description, data.hours])
+    return await Subject.getSubject({subjectId: result.insertId})
+}
+
+Subject.updateSubject = async (data) => {
+    await dbConn.query("UPDATE SUBJECT SET " +
+    "name=COALESCE(?,name), description=COALESCE(?,description), hours=COALESCE(?,hours), " +
+    "alter_date=NOW() WHERE id=?", 
+    [data.name, data.description, data.hours, data.subjectId])
+    return await Subject.getSubject({subjectId: data.subjectId})
+}
+
+Subject.removeSubject = async (data) => {
+    const removed = Subject.getSubject({subjectId: data.subjectId})
+    await dbConn.query("DELETE FROM SUBJECT " +
+    "WHERE id=?", [data.subjectId])
+    return removed
+}
+
 module.exports = Subject
