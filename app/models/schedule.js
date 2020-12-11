ab464843b24dbe77234eec1a98933f50ed8a56d3
Jokūbas Setkauskas <graphframejokubas@gmail.com>
2020-12-11 14:15:40 +0200

    Added schedule and subject_user related CRUD ops

--- app/models/schedule.js
+++ app/models/schedule.js
@@ -10,4 +10,27 @@ Schedule.getScheduleInfo = async (data) => {
     return info[0]
 }
 
+Schedule.createSchedule = async (data) => {
+    const [result,fields] = await dbConn.query("INSERT INTO SCHEDULE " +
+    "(name, description) VALUES " +
+    "(?,?)", [data.name, data.description])
+    return await Subject.getSubject({scheduleId: result.insertId})
+}
+
+Schedule.updateSchedule = async (data) => {
+    await dbConn.query("UPDATE SCHEDULE SET " +
+    "name=COALESCE(?,name), description=COALESCE(?,description), " +
+    "alter_date=NOW() WHERE id=?", 
+    [data.name, data.description, data.scheduleId])
+    return await Subject.getSubject({scheduleId: data.scheduleId})
+}
+
+Schedule.removeSchedule = async (data) => {
+    const removed = Subject.getSubject({scheduleId: data.scheduleId})
+    await dbConn.query("DELETE FROM SCHEDULE " +
+    "WHERE id=?", [data.scheduleId])
+    return removed
+}
+
+
 module.exports = Schedule
--- app/models/subject_user.js
+++ app/models/subject_user.js
@@ -5,10 +5,38 @@ const dbConn = require('../db');
 var SubjectUser = function(subjectUser) {}
 
 SubjectUser.getSubjects = async (data) => {
-    const [subjects,fields] = await dbConn.query("SELECT SUBJECT.* FROM SUBJECT_USER " +
+    const [result,fields] = await dbConn.query("SELECT SUBJECT.* FROM SUBJECT_USER " +
     "LEFT JOIN SUBJECT ON SUBJECT.id=fk_subjectId " + 
     "WHERE fk_userId=?", [data.userId])
-    return subjects
+    return result
+}
+
+SubjectUser.getSubjectUser = async (data) => {
+    const [result,fields] = await dbConn.query("SELECT * FROM SUBJECT_USER " +
+    "WHERE id=?", [data.id])
+    return result[0]
+}
+
+SubjectUser.createSubjectUser = async (data) => {
+    const [result,fields] = await dbConn.query("INSERT INTO SUBJECT_USER " +
+    "(fk_subjectId, fk_userId) VALUES " +
+    "(?,?)", [data.subjectId, data.userId])
+    return await SubjectUser.getSubjectUser({id: result.insertId})
+}
+
+SubjectUser.updateSubjectUser = async (data) => {
+    await dbConn.query("UPDATE SUBJECT_USER SET " +
+    "fk_subjectId=COALESCE(?,fk_subjectId), fk_userId=COALESCE(?,fk_userId) " +
+    "WHERE id=?", 
+    [data.subjectId, data.userId, data.id])
+    return await SubjectUser.getSubjectUser({id: data.id})
+}
+
+SubjectUser.removeSubjectUser = async (data) => {
+    const removed = SubjectUser.getSubjectUser({id: data.id})
+    await dbConn.query("DELETE FROM SUBJECT_USER " +
+    "WHERE id=?", [data.id])
+    return removed
 }
 
 module.exports = SubjectUser
