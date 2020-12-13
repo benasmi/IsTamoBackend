801a8e05f42ef366a0d9fa63c906152a4d0cedfd
Jokūbas Setkauskas <graphframejokubas@gmail.com>
2020-12-13 12:50:56 +0200

    Modified Subject_User api GET endpoint

--- app/routes/subjectRoutes.js
+++ app/routes/subjectRoutes.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const router = new express.Router()
 const SubjectUser = require('../models/subject_user')
+const Subject = require('../models/subject_user')
 const auth = require('../middleware/auth.js')
 
 router.get('/users', auth, async (req, res) => {
-    if (!req.body || !req.body.userId || !req.body.subjectId)
-        return res.status(400).send({error: true, message: "Bad params"})
-
     try {
-        const result = await SubjectUser.findSubjectUser(req.body)
+        const result = await SubjectUser.getSubjects(req.body)
         return res.status(200).send(result)
     } catch (e) {return res.status(500).send({error: true, message: e.message})}
 })
